7070b0e4110b38d4125a52208a55737b2d75a78a
dhlee <dhlee.work@gmail.com>
2025-05-20 21:14:38 +0900

    init

--- B1.BuildModel.py
+++ B1.BuildModel.py
@@ -63,7 +63,7 @@ def load_logger(config):
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default='./config/experiment34.yaml', help='Path to config file')
+    parser.add_argument("--config", type=str, default='./config/experiment40.yaml', help='Path to config file')
     parser.add_argument("--mode", type=str, default=None)
     parser.add_argument("--host", type=str, default=None)
     parser.add_argument("--port", type=str, default=None)
