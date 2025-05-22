452f7a406ea2017e218b13fab6ff24ddc122849c
dhlee <dhlee.work@gmail.com>
2025-05-22 17:11:12 +0900

    init

--- src/datautils.py
+++ src/datautils.py
@@ -57,7 +57,7 @@ class Fathomnet_Dataset(Dataset):
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             transforms.RandomApply([
-                transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)
+                transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2)
             ], p=0.99),
             transforms.RandomRotation(degrees=15, interpolation=InterpolationMode.BILINEAR, expand=False),
             # transforms.RandomGrayscale(p=0.1),
